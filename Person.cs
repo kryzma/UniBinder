0f1a8617f7ee2157ee74a4e76ed90aff13f677f5
Skullcr1 <kornelismak@gmail.com>
2019-09-23 18:28:35 +0300

    Separated 2 classes: Person and DataReader. Created separated class DataReader, whose only purpose is to import data.

--- Person.cs
+++ Person.cs
@@ -12,34 +12,16 @@ namespace ConsoleApp1
         public int ImgId { get; set; }
         //[JsonProperty("list")]
         public List<Subject> Subjects { get; set; }
-        static readonly string TextFile = "../../data.json";
+        
 
         static void Main(string[] args)
         {
             //string textFile = "../../data.json";
-            Person l = new Person();
-            l.UploadData();
+            DataReader dr = new DataReader();
+            dr.UploadData();
 
 
         }
-        void UploadData()
-        {
-            List<Person> people = new List<Person>();
-            string jsons = File.ReadAllText(TextFile);
-            //string jsonS = @"[{""Name"":""John"",""Age"":25,""Subjects"":[{""Name"":""maths""}]},{""Name"":""j"",""Age"":40,""Subjects"":[{""Name"":""physics""}]}]"; //some testing data in case .json file would not work
-            var PersonList = JsonConvert.DeserializeObject<List<Person>>(jsons);
-
-            /*
-            List contains deserialized objects from .json file.             
-            */
-
-            foreach (var person in PersonList) 
-            {
-                people.Add(person);
-                Console.WriteLine(person.Name);
-                Console.WriteLine(person.Age);
-                Console.WriteLine(person.Subjects[0].SubjectName);
-            }
-        }
+        
     }
 }
