24af33e743a6fa3943da6201e9596e9c18366b7f
Accir <44165418+Accir@users.noreply.github.com>
2019-10-01 00:09:46 +0300

    Implemented loading from JSON

---
+++ GUI/GUI/Person.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GUI
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ImgId { get; set; }
+        public int HelpScore { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public int PeopleHelped { get; set; }
+        //[JsonProperty("list")]
+        public List<Subject> Subjects { get; set; }
+        
+    }
+}
---
+++ GUI/GUI/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace GUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /// Start GUI
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            UniBinder GUI = new UniBinder();
+            LoadValues(GUI);
+            GUI.InitialLoad();
+            Application.Run(GUI);
+     
+        }
+        static void LoadValues(object gui)
+        {
+            ////data = dr.UploadData();
+            ////int dataLenght = data.Count;
+            ////Person current = new Person();
+            ////current = data[0];
+            ////NameLabel.Text = current.Name;
+            ////LoadSubjects();
+
+            //int index = 1;
+            //DataReader dr = new DataReader();
+            //List<Person> data;
+
+            //data = dr.UploadData();
+            //int dataLenght = data.Count;
+            //Person current = new Person();
+            //current = data[0];
+            //NameLabel.Text = current.Name;
+
+        }
+    }
+}
