cba7a175d952d8310bfc81eecc8546ed8da75cbb
Skullcr1 <44177622+Skullcr1@users.noreply.github.com>
2019-09-30 18:22:49 +0300

    added project file

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ImgId { get; set; }
+        //[JsonProperty("list")]
+        public List<Subject> Subjects { get; set; }
+        
+
+        static void Main(string[] args)
+        {
+            //string textFile = "../../data.json";
+            DataReader dr = new DataReader();
+            dr.UploadData();
+
+
+        }
+        
+    }
+}
