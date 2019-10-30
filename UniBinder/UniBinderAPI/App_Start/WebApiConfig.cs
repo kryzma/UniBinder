8d24611eaa7e0a99a713ed1997ae2f7b24f40110
Maksimowicz Korneliusz Tomasz <korneliusz.maksimowicz@mif.stud.vu.lt>
2019-10-30 23:04:47 +0200

    added Cors

--- UniBinder/UniBinderAPI/App_Start/WebApiConfig.cs
+++ UniBinder/UniBinderAPI/App_Start/WebApiConfig.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace UniBinderAPI
 {
+
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.EnableCors();
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+    .Add(new MediaTypeHeaderValue("text/html"));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
