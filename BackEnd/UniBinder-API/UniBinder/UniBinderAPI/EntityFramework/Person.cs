5aa5d92aa986c30a1ec24169f7bbebdf13759b38
Maksimowicz Korneliusz Tomasz <korneliusz.maksimowicz@mif.stud.vu.lt>
2019-11-04 20:16:57 +0200

    Added initial EF setup

---
+++ BackEnd/UniBinder-API/UniBinder/UniBinderAPI/EntityFramework/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UniBinderAPI.EntityFramework
+{
+    public class Person
+    {
+        [Key]
+        public int PersobID { get; set; }
+        public string PersonName { get; set; }
+    }
+}
