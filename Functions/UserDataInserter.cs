390b40e40b8b1678dc7c45ca1150e2e28981febe
kryzma <48096081+kryzma@users.noreply.github.com>
2019-11-06 09:09:41 +0200

    Added UserDataReader and UserDataInserter functions

---
+++ Functions/UserDataInserter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogIn
+{
+    class UserDataInserter : IUserDataInserter
+    {
+        public void SendUserData(Person person)
+        {
+            var context = new studybuddyEntities();
+
+            context.Person.Add(person);
+            context.SaveChanges();
+        }
+    }
+
+}
---
+++ Functions/UserDataReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogIn
+{
+    class UserDataReader : IUserDataReader
+    {
+        List<Person> PersonList = new List<Person>();
+
+        public List<Person> GetUserList()
+        {
+            if (PersonList.Count == 0)
+            {
+                return ReadUserData();
+            }
+            else
+            {
+                return PersonList;
+            }
+        }
+        public List<Person> ReadUserData()  
+        {
+            var context = new studybuddyEntities();
+
+            PersonList = (from a in context.Person
+                          select a
+                          ).ToList();
+
+            var result = (from a in context.PersonSubject
+                          select a
+                          ).ToList();
+
+            result.ForEach(subject =>
+            {
+                PersonList.ForEach(person =>
+                {
+                    if (subject.ID.Equals(person.ID))
+                    {
+                        person.subjectList.Add(new Subject(subject.Name));
+                    }
+                });
+            });
+
+            context.Dispose();
+            return PersonList;
+        }
+    }
+}
