2f156c1edd7baa90e059f9741e387098ad4e4131
Maksimowicz Korneliusz Tomasz <korneliusz.maksimowicz@mif.stud.vu.lt>
2019-11-06 19:55:41 +0200

    added exceptions for unknown data

--- BackEnd/UniBinder-API/UniBinder/UniBinderAPI/Controllers/PersonController.cs
+++ BackEnd/UniBinder-API/UniBinder/UniBinderAPI/Controllers/PersonController.cs
@@ -18,14 +18,10 @@ namespace UniBinderAPI.Controllers
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class PersonController : ApiController
     {
-        List<Credentials> credentials = new List<Credentials>();
         UserDataReader userDataReader = new UserDataReader();
 
         PersonController()
-        {
-
-            ///Iskelti
-            
+        {           
         }
 
         [Route("api/person/count")]
@@ -42,11 +38,15 @@ namespace UniBinderAPI.Controllers
         }
 
         // GET: api/Person/5
-        public Person Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            return userDataReader.ReadUserData().Where(x => x.ID == id).FirstOrDefault();
+            var userID = userDataReader.ReadUserData().Where(x => x.ID == id).FirstOrDefault();
+            if(userID == null)
+            {
+                UnknownData(id.ToString(), "ID");
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, userID);
         }
-
 
         [Route("api/person/ID")]
         [HttpGet]
@@ -56,35 +56,27 @@ namespace UniBinderAPI.Controllers
             return RetrieveID(username);
         }
 
-        private int RetrieveID(string username)
-        {
-            var p2 = userDataReader.ReadUserData().Where(x => x.Name == username).FirstOrDefault().ID;
-            return p2;
-        }
+
 
         [Route("api/person/Pass")]
         [HttpGet]
-       // [AllowAnonymous]
-        public string getPassword(string username)
+        // [AllowAnonymous]
+        public HttpResponseMessage getPassword(string username)
         {
             List<Person> people = userDataReader.ReadUserData();
             var person = people.Where(x => x.Name == username).ToList().FirstOrDefault();
-            return person.Password;
-            throw new HttpResponseException(HttpStatusCode.Unauthorized);
-        }
-
-        // DELETE: api/Person/5
-        public void Delete(int id)
-        {
+            if (person == null) UnknownData(username, "Username");
+            return Request.CreateResponse(HttpStatusCode.OK, person);
         }
 
         [Route("api/person/Token")]
         [HttpGet]
        // [AllowAnonymous]
-        public string GetToken(string username)
+        public HttpResponseMessage GetToken(string username)
         {
             List<Person> people = userDataReader.ReadUserData();
             var personCredentials = people.Where(x => x.Name == username).ToList().FirstOrDefault();
+            if (personCredentials == null) UnknownData(username, "Username");
             IAuthContainerModel model = GetJWTContainerModel(username, RetrieveID(username).ToString());
             IAuthService authService = new JWTService(model.SecretKey);
             string token = authService.GenerateToken(model);
@@ -97,7 +89,7 @@ namespace UniBinderAPI.Controllers
                 //System.Diagnostics.Debug.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Name)).Value);
                 //System.Diagnostics.Debug.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Email)).Value);
                 //System.Diagnostics.Debug.WriteLine(token);
-                return token;
+                return Request.CreateResponse(HttpStatusCode.OK, token);
             }
         }
 
@@ -113,6 +105,17 @@ namespace UniBinderAPI.Controllers
                     //new Claim(ClaimTypes.Email, password)
                 }
             };
+        }
+
+        private int RetrieveID(string username)
+        {
+            var p2 = userDataReader.ReadUserData().Where(x => x.Name == username).FirstOrDefault().ID;
+            return p2;
+        }
+        private HttpResponseMessage UnknownData(string data, string nameOfData)
+        {
+            string message = string.Format("No User found with {0} = {1}", nameOfData, data);
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, message);
         }
         #endregion
 
