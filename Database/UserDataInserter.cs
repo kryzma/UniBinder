ac5d7dbffa53ef8d70183db98f563ef976ae886b
kryzma <48096081+kryzma@users.noreply.github.com>
2019-11-12 14:11:43 +0200

    Fixed dataReaderQuery

---
+++ Database/UserDataInserter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniBinderAPI.Models;
+
+namespace UniBinderAPI.Database
+{
+
+        class UserDataInserter : IUserDataInserter
+        {
+            public void SendUserData(Person person)
+            {
+                using (studybuddyEntities context = new studybuddyEntities())
+                {
+                    var context = new studybuddyEntities();
+                    context.Person.Add(person);
+                    context.SaveChanges();
+                }   
+            }
+        }
+
+    
+
+}
