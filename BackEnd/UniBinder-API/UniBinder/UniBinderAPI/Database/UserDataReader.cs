ef6d0cd8f6671fd710ba01e338e221fa9f89eaa6
Maksimowicz Korneliusz Tomasz <korneliusz.maksimowicz@mif.stud.vu.lt>
2019-11-21 01:51:04 +0200

    added threads to database reader

--- BackEnd/UniBinder-API/UniBinder/UniBinderAPI/Database/UserDataReader.cs
+++ BackEnd/UniBinder-API/UniBinder/UniBinderAPI/Database/UserDataReader.cs
@@ -5,6 +5,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using UniBinderAPI.EntityFramework;
 using UniBinderAPI.Models;
@@ -27,60 +28,68 @@ namespace UniBinderAPI.Database
                 return PersonList;
             }
         }
-        public List<Person> ReadUserData()
-        {
 
-            using (UniBinderEF context = new UniBinderEF())
+
+            public List<Person> ReadUserData()
             {
-                var users = (from a in context.People select a).ToList();
-                var usersSubjects = (from a in context.PersonSubjects select a).ToList();
 
-                var groupJoin = users.GroupJoin(usersSubjects,
-                    per => per.ID,
-                    sub => sub.PersonID,
-                    (per, subjectGroup) => new
-                    {
-                        subjectsList = subjectGroup,
-                        per.ID,
-                        per.Username,
-                        per.Password,
-                        per.Name,
-                        per.Surname,
-                        per.Email,
-                        per.Role,
-                        per.Likes,
-                        per.Dislikes,
-                        per.ImageLink,
-
-                    });
-
-                foreach (var per in groupJoin)
+                Thread thread = new Thread(() =>
                 {
-                    Person person = new Person();
-                    person.ID = per.ID;
-                    person.Username = per.Username;
-                    person.Password = per.Password;
-                    person.Name = per.Name;
-                    person.Surname = per.Surname;
-                    person.Email = per.Email;
-                    person.Role = per.Role;
-                    person.Likes = per.Likes;
-                    person.Dislikes = per.Dislikes;
-                    person.ImageLink = per.ImageLink;
-
-                    person.SubjectList = new List<Subject>();
-                    foreach (var sub in per.subjectsList)
+                    using (UniBinderEF context = new UniBinderEF())
                     {
-                        Subject subject = new Subject();
-                        subject.Name = sub.Name;
-                        person.SubjectList.Add(subject);
-                    }
-                    PersonList.Add(person);
-                }
+                        var users = (from a in context.People select a).ToList();
+                        var usersSubjects = (from a in context.PersonSubjects select a).ToList();
 
+                        var groupJoin = users.GroupJoin(usersSubjects,
+                        per => per.ID,
+                        sub => sub.PersonID,
+                        (per, subjectGroup) => new
+                        {
+                            subjectsList = subjectGroup,
+                            per.ID,
+                            per.Username,
+                            per.Password,
+                            per.Name,
+                            per.Surname,
+                            per.Email,
+                            per.Role,
+                            per.Likes,
+                            per.Dislikes,
+                            per.ImageLink,
+
+                        });
+
+                        foreach (var per in groupJoin)
+                        {
+                            Person person = new Person();
+                            person.ID = per.ID;
+                            person.Username = per.Username;
+                            person.Password = per.Password;
+                            person.Name = per.Name;
+                            person.Surname = per.Surname;
+                            person.Email = per.Email;
+                            person.Role = per.Role;
+                            person.Likes = per.Likes;
+                            person.Dislikes = per.Dislikes;
+                            person.ImageLink = per.ImageLink;
+
+                            person.SubjectList = new List<Subject>();
+                            foreach (var sub in per.subjectsList)
+                            {
+                                Subject subject = new Subject();
+                                subject.Name = sub.Name;
+                                person.SubjectList.Add(subject);
+                            }
+                            PersonList.Add(person);
+                        }
+                    }
+                });
+                thread.Start();
+                thread.Join();
                 return PersonList;
+
             }
-        }
+        
 
 
         public bool CheckUniqueData(string username, string email)
