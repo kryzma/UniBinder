601ab4266b56e5aba42d469a58d52331058800b6
Skullcr1 <kornelismak@gmail.com>
2019-12-11 01:29:48 +0200

    added ImgHandler class (for uploading/retrieving pic)

---
+++ BackEnd/UniBinder-API/UniBinder/UniBinderAPI/FileManager/ImgHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace UniBinderAPI.FileManager
+{
+    public class ImgHandler
+    {
+        public string ImgBase64{ get; set; }
+        public string ImgPath{ get; set; }
+    }
+}
